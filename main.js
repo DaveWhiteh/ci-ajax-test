6495e3fd4c3d90cde8e0131042103075b6c56723
DaveWhiteh <david_whiteh@msn.com>
2023-06-02 20:42:10 +0000

    Amend javascript code adding in callback function

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
+// var data;
 
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+// xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+// xhr.send();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+// xhr.onreadystatechange = function() {
+//     if(this.readyState == 4 && this.status == 200) {
+//         // document.getElementById("data").innerHTML = this.responseText; // add response to html div
+//         // console.log(JSON.parse(this.responseText)); // parse the json as an object
+//         data = JSON.parse(this.responseText);
+//     }
+// };
 
-xhr.send();
+// setTimeout(function() {
+//     console.log(data);
+// }, 500);
+
+
+
+
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // document.getElementById("data").innerHTML = this.responseText; // add response to html div
+            // console.log(JSON.parse(this.responseText)); // parse the json as an object
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
